73233c3e0073e4fd1754f24aef16f3cb1005a658
zeyun.gong <bangliutishi@163.com>
2018-11-22 06:53:34 -0800

    1) create sdl window
    Signed-off-by: zeyun.gong <bangliutishi@163.com>

--- player.c
+++ player.c
@@ -8,6 +8,56 @@
 
 static int file_index = 0;
 
+typedef struct VideoRender{
+	SDL_Window* window;
+	SDL_Renderer* render;
+	SDL_Texture* texture;
+	int format;
+	int x;
+	int y;
+	int width;
+	int height;
+}VideoRender;
+
+typedef struct AudioRender {
+	
+}AudioRender;
+
+
+typedef struct AVPlayer {
+	VideoRender *video_render;
+	AudioRender *audio_render;
+}AVPlayer;
+
+
+static const struct TextureFormatEntry {
+    enum AVPixelFormat format;
+    int texture_fmt;
+} sdl_texture_format_map[] = {
+    { AV_PIX_FMT_RGB8,           SDL_PIXELFORMAT_RGB332 },
+    { AV_PIX_FMT_RGB444,         SDL_PIXELFORMAT_RGB444 },
+    { AV_PIX_FMT_RGB555,         SDL_PIXELFORMAT_RGB555 },
+    { AV_PIX_FMT_BGR555,         SDL_PIXELFORMAT_BGR555 },
+    { AV_PIX_FMT_RGB565,         SDL_PIXELFORMAT_RGB565 },
+    { AV_PIX_FMT_BGR565,         SDL_PIXELFORMAT_BGR565 },
+    { AV_PIX_FMT_RGB24,          SDL_PIXELFORMAT_RGB24 },
+    { AV_PIX_FMT_BGR24,          SDL_PIXELFORMAT_BGR24 },
+    { AV_PIX_FMT_0RGB32,         SDL_PIXELFORMAT_RGB888 },
+    { AV_PIX_FMT_0BGR32,         SDL_PIXELFORMAT_BGR888 },
+    { AV_PIX_FMT_NE(RGB0, 0BGR), SDL_PIXELFORMAT_RGBX8888 },
+    { AV_PIX_FMT_NE(BGR0, 0RGB), SDL_PIXELFORMAT_BGRX8888 },
+    { AV_PIX_FMT_RGB32,          SDL_PIXELFORMAT_ARGB8888 },
+    { AV_PIX_FMT_RGB32_1,        SDL_PIXELFORMAT_RGBA8888 },
+    { AV_PIX_FMT_BGR32,          SDL_PIXELFORMAT_ABGR8888 },
+    { AV_PIX_FMT_BGR32_1,        SDL_PIXELFORMAT_BGRA8888 },
+    { AV_PIX_FMT_YUV420P,        SDL_PIXELFORMAT_IYUV },
+    { AV_PIX_FMT_YUYV422,        SDL_PIXELFORMAT_YUY2 },
+    { AV_PIX_FMT_UYVY422,        SDL_PIXELFORMAT_UYVY },
+    { AV_PIX_FMT_NONE,           SDL_PIXELFORMAT_UNKNOWN },
+};
+
+AVPlayer player;
+
 int dump_frame(AVFrame* frame)
 {
     char file_path[256] = {0};
@@ -29,16 +79,28 @@ int dump_frame(AVFrame* frame)
     fclose(pFile);
 }
 
+
+static int create_sdl_window()
+{
+    SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("test", 0, 0, 1920, 1080, SDL_WINDOW_RESIZABLE);
+	//player->video_render->window = window;
+}
+
 int main(int argc, char* argv[])
 {
     AVFormatContext *pFormatContext = NULL;
-    //SDL_Surface *test = NULL;
-    //SDL_Init(SDL_INIT_EVERYTHING);
+    
     if (argc < 2) {
        av_log(pFormatContext, AV_LOG_ERROR, "You should specify file path to open");
        return -1;
     }
-    
+	
+    create_sdl_window();
+    while (1) {
+        usleep(100 * 1000);
+    }
+#if 0	
     const char* file_name = argv[1];
     av_log(pFormatContext, AV_LOG_ERROR, "open file ====%s====\n", file_name);
 
@@ -105,6 +167,7 @@ fail:
     avcodec_free_context(&pCodecContext);
     av_frame_free(frame);
     avformat_close_input(&pFormatContext);
+#endif
     return 0;
 }
 
