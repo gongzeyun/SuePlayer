a4bfb0ee763d724f5a3bab298ca21cb20bc15993
zeyun.gong <bangliutishi@163.com>
2019-07-09 08:21:53 -0700

    create mainthread loop
    Signed-off-by: zeyun.gong <bangliutishi@163.com>

--- player.c
+++ player.c
@@ -28,8 +28,9 @@ typedef struct AVPlayer {
 	VideoRender *video_render;
 	AudioRender *audio_render;
 
-        AVFormatContext *context;
-        AVCodecContext* vcodec_context;
+    AVFormatContext *context;
+    AVCodecContext* vcodec_context;
+	
 	/* video info */
 	int video_width;
 	int video_height;
@@ -38,6 +39,10 @@ typedef struct AVPlayer {
 	int audio_samplerate;
 	int audio_channels;
 	int audio_format;
+
+    SDL_Thread* main_thread;
+    SDL_Thread* video_refresh;
+    SDL_Thread* audio_refresh;        
 	
 }AVPlayer;
 
@@ -96,7 +101,7 @@ int dump_frame(AVFrame* frame)
 
 
 
-static int create_sdl_window(AVPlayer* player, int width, int height, int pixel_format)
+static int create_video_render(int width, int height, int pixel_format)
 {
     SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
     
@@ -106,6 +111,7 @@ static int create_sdl_window(AVPlayer* player, int width, int height, int pixel_
 
 }
 
+static int create_audio_render(int channels, )
 static int open_video_decoder(AVFormatContext* context) {
 
     int ret = -1;
@@ -138,9 +144,95 @@ static int streams_open(AVFormatContext **context, const char*name) {
         return ret;
     }
 
-    return avformat_find_stream_info(*context, NULL);
+    ret = avformat_find_stream_info(*context, NULL);
+    if (ret < 0) {
+        avformat_close_input(*context);
+        av_log(context, AV_LOG_ERROR, "%s, find stream info failed\n", __func__);
+        return ret;
+    }
+
+    player.video_width = (*context)->streams[AVMEDIA_TYPE_VIDEO]->codecpar->width;
+    player.video_height = (*context)->streams[AVMEDIA_TYPE_VIDEO]->codecpar->height;
+
+    ret = open_video_decoder(*context);
+    if (ret < 0) {
+        av_log(context, AV_LOG_ERROR, "%s, open video decoder failed\n", __func__);
+        avformat_close_input(*context);
+        return ret;
+    }
+
+    return ret;
 }
 
+static int streams_close() {
+    if (player.vcodec_context != NULL) {
+	    avcodec_close(player.vcodec_context);
+        avcodec_free_context(&player.vcodec_context);
+		player.vcodec_context = NULL;
+    }
+	if (player.context != NULL) {
+        avformat_close_input(&player.context);
+	}
+
+    return 0;
+}
+
+static int render_video_frame(AVFrame* frame) {
+    SDL_Event event;
+	if (frame) {
+		SDL_PollEvent(&event);
+	    SDL_UpdateYUVTexture(sdlTexture, NULL, frame->data[0], frame->linesize[0],
+	                              frame->data[1], frame->linesize[1],
+	                              frame->data[2], frame->linesize[2]);
+	    SDL_Rect sdlRect;
+	    sdlRect.x = 0;  
+	    sdlRect.y = 0;  
+	    sdlRect.w = player.video_width;  
+	    sdlRect.h = player.video_height;
+	    SDL_RenderClear(sdlRenderer);
+	    SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
+	    SDL_RenderPresent(sdlRenderer);
+    }
+}
+
+
+static void main_threadloop(AVFormatContext* context) {
+    int ret = -1;
+    AVFrame *frame = NULL;
+    AVPacket pkt;
+	
+    frame = av_frame_alloc();
+    while (frame != NULL) {
+        int read_ret = av_read_frame(player.context, &pkt);
+        if (read_ret < 0) {
+            av_log(player.context, AV_LOG_ERROR, "read packet failed, ret:%d\n", ret);
+            break;
+        }
+    
+        if (pkt.stream_index == AVMEDIA_TYPE_VIDEO) {
+            int ret_send_pkt = avcodec_send_packet(player.vcodec_context, &pkt);
+            int ret_decoder = avcodec_receive_frame(player.vcodec_context, frame);
+            if (ret_decoder >= 0) {
+                render_video_frame(frame);
+                av_frame_unref(frame);
+                usleep(40 * 1000);
+            }
+        } 
+        av_packet_unref(&pkt);
+    }
+    av_frame_free(frame);
+
+	streams_close();
+    return;
+}
+
+
+
+static int event_loop() {
+    while (1) {
+        usleep(100 * 1000);
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -149,10 +241,7 @@ int main(int argc, char* argv[])
        av_log(NULL, AV_LOG_ERROR, "You should specify file to open");
        return -1;
     }
-
-    SDL_Event event;
     
-#if 1	
     const char* file_name = argv[1];
     av_log(NULL, AV_LOG_ERROR, "====source:%s\n", file_name);
 
@@ -162,58 +251,18 @@ int main(int argc, char* argv[])
     ret = streams_open(&player.context, file_name);
     if (ret < 0) {
         av_log(NULL, AV_LOG_ERROR, "====open %s failed, goto fail\n", file_name);
-        goto fail;
+		return ret;
     }
     av_dump_format(player.context, 0, file_name, 0);
 
 
-    int video_width = player.context->streams[AVMEDIA_TYPE_VIDEO]->codecpar->width;
-    int video_height = player.context->streams[AVMEDIA_TYPE_VIDEO]->codecpar->height;
-    create_sdl_window(NULL, video_width, video_height, SDL_PIXELFORMAT_IYUV);
-
-    AVFrame *frame = av_frame_alloc();
     
-    if (0 == open_video_decoder(player.context)) {
-#if 1
-        AVPacket pkt;
-        while (frame != NULL) {
-            int read_ret = av_read_frame(player.context, &pkt);
-            if (read_ret < 0) {
-                av_log(player.context, AV_LOG_ERROR, "read packet failed\n");
-                break;
-            }
-            
-            if (pkt.stream_index == AVMEDIA_TYPE_VIDEO) {
-                int ret_send_pkt = avcodec_send_packet(player.vcodec_context, &pkt);
-                int ret_decoder = avcodec_receive_frame(player.vcodec_context, frame);
-                if (ret_decoder >= 0) {
-                    //dump_frame(frame);
-                    SDL_PollEvent(&event);
-                    SDL_UpdateYUVTexture(sdlTexture, NULL, frame->data[0], frame->linesize[0],
-                                                  frame->data[1], frame->linesize[1],
-                                                  frame->data[2], frame->linesize[2]);
-                    SDL_Rect sdlRect;
-                    sdlRect.x = 0;  
-                    sdlRect.y = 0;  
-                    sdlRect.w = 1920;  
-                    sdlRect.h = 1080;
-                    SDL_RenderClear(sdlRenderer);
-                    SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
-                    SDL_RenderPresent(sdlRenderer);
-                    av_frame_unref(frame);
-                    usleep(40 * 1000);
-                }
-            } 
-            av_packet_unref(&pkt);
-        }
-    }
-#endif
-fail:
-    avcodec_close(player.vcodec_context);
-    avcodec_free_context(&player.vcodec_context);
-    av_frame_free(frame);
-    avformat_close_input(&player.context);
-#endif
+    create_video_render(player.video_width, player.video_height, SDL_PIXELFORMAT_IYUV);
+
+    //create main thread
+    player.main_thread = SDL_CreateThread(main_threadloop, "main_threadloop", player.context);
+
+    event_loop();
     return 0;
 }
 
